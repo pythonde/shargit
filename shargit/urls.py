dcc4674640f6d6f521cbd75cb3a47bb1b2124490
xiaoming <3446263218@qq.com>
2018-09-14 17:23:01 +0800

    修改urls.py跟路由

--- shargit/urls.py
+++ shargit/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^stu1/', views.index_view),
 ]
